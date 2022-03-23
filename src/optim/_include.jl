947b393639ccf3768f1c8c274fb730b26a2542bf
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-03-23 20:22:35 -0300

    fix

--- src/optim/_include.jl
+++ src/optim/_include.jl
@@ -2,5 +2,6 @@ include("partitionedKnetnlpmodels.jl")
 include("utils.jl")
 include("methods.jl")
 include("lbfgs.jl") # à revoir
+include("partitioned_update_solver.jl")
 # include("plbfgs.jl")
 # include("plsr1.jl")
