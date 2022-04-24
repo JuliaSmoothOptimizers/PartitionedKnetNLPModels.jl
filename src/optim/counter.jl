fb2188305b6f13c8792bb170cf3f09806ccea407
RAYNAUD Paul (raynaudp) <raynaudp@grenoble-inp.lan>
2022-04-23 22:44:02 -0300

    small fixes

--- src/optim/counter.jl
+++ src/optim/counter.jl
@@ -3,4 +3,4 @@ mutable struct Counter_accuracy{T}
 end
 
 Counter_accuracy(T :: DataType) = Counter_accuracy{T}(Vector{T}(undef,0))
-push_acc!(counter :: Counter_accuracy{T}, value :: T) where T = push!(counter.acc, value :: T)
+push_acc!(counter :: Counter_accuracy{T}, value :: Y) where {T, Y<:Number} = push!(counter.acc, (Y)(value))
